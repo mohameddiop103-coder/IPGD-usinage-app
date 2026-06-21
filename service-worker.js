3b4ad0fddcd7e859bfaab5811418f181836d9c55
mohameddiop103-coder <mohameddiop103@gmail.com>
2026-06-21 21:08:15 +0000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ipgd-cache-v1";
+const CACHE_NAME = "ipgd-cache-v3";
 const ASSETS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function (event) {
